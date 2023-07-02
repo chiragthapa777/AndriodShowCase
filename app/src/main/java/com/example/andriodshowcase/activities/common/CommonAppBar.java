4d52953186a4357213081140330b2863312d1d3c
chiragthapa777 <chiragthapa777@gmail.com>
2023-07-02 12:28:36 +0545

    linear layout completed

---
+++ app/src/main/java/com/example/andriodshowcase/activities/common/CommonAppBar.java
@@ -0,0 +1,70 @@
+package com.example.andriodshowcase.activities.common;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.andriodshowcase.R;
+import com.example.andriodshowcase.activities.layout.LinearLayoutExample;
+import com.google.android.material.navigation.NavigationView;
+
+public class CommonAppBar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_app_bar);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.linear_layout:{
+                Intent intent = new Intent(this, LinearLayoutExample.class);
+                startActivity(intent);
+                break;
+            }
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/andriodshowcase/activities/layout/LinearLayoutExample.java
@@ -0,0 +1,17 @@
+package com.example.andriodshowcase.activities.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.andriodshowcase.R;
+import com.example.andriodshowcase.activities.common.CommonAppBar;
+
+public class LinearLayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout_example);
+    }
+}
